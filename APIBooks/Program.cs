0027adc8745603f4723175e17698dfeb68578fa5
DimaNaumov <157685429+DimaNaumiv@users.noreply.github.com>
2026-03-27 23:30:56 +0200

    Added swagger for a production

--- APIBooks/Program.cs
+++ APIBooks/Program.cs
@@ -63,11 +63,8 @@ namespace APIBooks
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
